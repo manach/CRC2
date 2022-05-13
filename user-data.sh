e25a1e71530eaec3bfa45b955322f7a19c13ad11
Chris Scott <chris.scott22@gmail.com>
2022-05-12 21:42:07 -0500

    adds sudo to userdata

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
